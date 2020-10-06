0b0cf2233c9b1b9f8cb2d61b8f434788d499c87a
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-10-06 14:57:40 -0300

    resolvido exercicios principais

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,3 +1,36 @@
-function criarTarefa(){
-    
+function criarTarefa() {
+    const inputTarefa = document.getElementById("tarefa");
+    let tarefa = inputTarefa.value;
+    //console.log(tarefa);
+    const diasSemana = document.getElementById("dias-semana");
+    let semana = diasSemana.value;
+    //console.log(semana);
+
+    if (inputTarefa.value === "") {
+        alert("Você precisa digitar alguma tarefa")
+    } else {
+        switch (semana) {
+            case "domingo":
+                document.getElementById("domingo").innerHTML += "<p>* " + tarefa + "</p>";
+                break;
+            case "segunda":
+                document.getElementById("segunda").innerHTML += "<p>* " + tarefa + "</p>";
+                break;
+            case "terca":
+                document.getElementById("terca").innerHTML += "<p>* " + tarefa + "</p>";
+                break;
+            case "quarta":
+                document.getElementById("quarta").innerHTML += "<p>* " + tarefa + "</p>";
+                break;
+            case "quinta":
+                document.getElementById("quinta").innerHTML += "<p>* " + tarefa + "</p>";
+                break;
+            case "sexta":
+                document.getElementById("sexta").innerHTML += "<p>* " + tarefa + "</p>";
+                break;
+            case "sabado":
+                document.getElementById("sabado").innerHTML += "<p>* " + tarefa + "</p>";
+                break;
+        }
+    }
 }
