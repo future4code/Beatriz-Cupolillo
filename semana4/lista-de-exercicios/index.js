8e0bea111570a6bb9f673325aabc996f632e65a0
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-10-09 15:07:48 -0300

    resolvido ex 1 e 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,1 +1,11 @@
 // Exercicios de Leitura de código
+
+//1 - A função conversor de moeda pergunta ao usuário qual o valor atual da cotação do dólar.
+// ele retornará o valor em Reais fazendo a multiplicação do valor desejado(100 dolares) * a cotação indicada.
+
+//2 - A função investeDinheiro possui algumas opções de investimento.
+// Cada opção retorna o valor aplicado * o quanto o investimento rende.
+// caso nao escolha nenhum dos investimentos possiveis, retornará um alerta para o usuário escolher um invest. válido.
+// Serão aplicados 150 em ações e 200 na poupança.
+// retornará (150*1.1) em ações e (200*1.03) na poupança. 
+
