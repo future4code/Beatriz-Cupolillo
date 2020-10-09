5e52435a415c5811d6469f8bce472d4b809555ff
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-10-09 16:08:29 -0300

    exercicio 4

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -9,7 +9,13 @@
 // Serão aplicados 150 em ações e 200 na poupança.
 // retornará (150*1.1) em ações e (200*1.03) na poupança. 
 
-// 3 - A função possui 1 array de numeros e 2 arrays vazios. O calculo é para saber se o número é par, se sim inclui-lo no
+// 3 - Existe 1 array de numeros e 2 arrays vazios. O calculo é para saber se o número é par, se sim inclui-lo no
 // array1 vazio, se for impar irá para o array2 vazio. 
 // depois mostrará a quantidade de numeros dentro do array original e dentro dos arrays de numeros pares e impares.
+
+// 4 - É declarado o valor de 3 arrays. 1 array de numeros, 1 de número infinito e um de valor 0.
+// o loop irá verificar se o primeiro array de numero é menor do que infinito, se sim, irá igualar o valor da variavel numero = numero1
+// o segundo verificará se o array 2 é maior do que o array valor 0, se sim, igualará os valores. 
+// será impresso o array inteiro no primeiro loop, pois nenhum número é maior que infinito.
+// no segundo será impresso o array inteiro exceto o -10, pois é menor que 0.
 
