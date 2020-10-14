3761ee46225183c040c9311b42e7347823a1299f
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-10-14 16:10:51 -0300

    finalizado

--- semana5/Labedin/src/App.js
+++ semana5/Labedin/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
 import ImagemButton from './components/ImagemButton/ImagemButton';
 import Beatriz from './img/Beatriz.jpg';
+import CardPequeno from './components/CardPequeno/CardPequeno';
+import TevaPharm from './img/TevaPharm.JPG';
+import Poliedro from './img/Poliedro.jfif';
 
 function App() {
   return (
@@ -14,6 +17,15 @@ function App() {
           nome="Beatriz Cupolillo" 
           descricao="Olá, eu sou a Beatriz. Tenho 26 anos e atualmente faço o curso de Web Full Stack na Labenu com o objetivo de me tornar uma desenvolvedora."
         />
+        <CardPequeno 
+          nome="Contatos" 
+          descricao="beatrizcupolillo@gmail.com"
+        />
+        <CardPequeno 
+        nome="Endereço" 
+        descricao="São Paulo/SP"
+        />
+
         
         <ImagemButton 
           imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
@@ -22,17 +34,32 @@ function App() {
       </div>
 
       <div className="page-section-container">
-        <h2>Experiências profissionais</h2>
+        <h2>Educação</h2>
         <CardGrande 
-          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
-          nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+          imagem="http://www.scs.usp.br/identidadevisual/wp-content/uploads/2013/08/usp-logo-eps.jpg"
+          nome="Universidade de São Paulo"
+          descricao="Bacharela em Letras - Português e Japonês" 
         />
         
         <CardGrande 
-          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
+          imagem="https://avatars2.githubusercontent.com/u/53058090?s=280"
+          nome="Labenu" 
+          descricao="Web Full Stack - Curso Integral" 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem={ TevaPharm } 
+          nome="Teva Farmacêutica" 
+          descricao="Assistente de Eventos" 
+        />
+        
+        <CardGrande 
+          imagem={ Poliedro } 
+          nome="Poliedro Educação" 
+          descricao="Corretora de Redações Pré-Vestibular" 
         />
       </div>
 
