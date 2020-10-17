a9f3eb97a2807637caa708854a17c79ff948e7ed
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-10-16 22:27:29 -0300

    Ex 1 - criado array para os posts

--- semana5/Insta4/src/App.js
+++ semana5/Insta4/src/App.js
@@ -3,28 +3,42 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    arrayPosts: [ 
+      {
+        nomeUsuario: 'paulinha',
+        fotoUsuario:'https://picsum.photos/50/50/?a=1',
+        fotoPost:'https://picsum.photos/200/150/?a=1'
+      },
+      {
+        nomeUsuario: 'bya',
+        fotoUsuario: 'https://picsum.photos/50/50/?a=2',
+        fotoPost: 'https://picsum.photos/200/150/?a=4'
+      },
+      {
+        nomeUsuario: 'nana',
+        fotoUsuario: 'https://picsum.photos/50/50/?a=3',
+        fotoPost: 'https://picsum.photos/200/150/?a=5'
+      }
+    ]
+  }
+
   render() {
-    return (
-      <div className={'app-container'}>
+    const listaDePosts = this.state.arrayPosts.map((post, index, posts) => {
+      return (
         <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50/?a=1'}
-          fotoPost={'https://picsum.photos/200/150/?a=1'}
+          nomeUsuario={post.nomeUsuario}
+          fotoUsuario={post.fotoUsuario}
+          fotoPost={post.fotoPost}
         />
-        <Post
-          nomeUsuario={'bya'}
-          fotoUsuario={'https://picsum.photos/50/50/?a=2'}
-          fotoPost={'https://picsum.photos/200/150/?a=4'}
-        />
-        <Post
-          nomeUsuario={'nana'}
-          fotoUsuario={'https://picsum.photos/50/50/?a=3'}
-          fotoPost={'https://picsum.photos/200/150/?a=5'}
-        />
-        
-      </div>
+      );
+    });
       
-    );
+      return (
+        <div className={'app-container'}>
+          {listaDePosts}
+        </div>
+      );
   }
 }
 
