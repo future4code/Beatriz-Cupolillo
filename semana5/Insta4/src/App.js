ff0f56ab9b8c2c30716028624c889ccb8f3e4f9f
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-10-16 23:09:46 -0300

    Feito ex2 - add formulario e alguns styled components

--- semana5/Insta4/src/App.js
+++ semana5/Insta4/src/App.js
@@ -1,27 +1,83 @@
-import React from 'react';
-import './App.css';
-import Post from './components/Post/Post';
+import React from "react";
+import "./App.css";
+import Post from "./components/Post/Post";
+import styled from "styled-components";
+
+const DivAppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: 250px;
+  padding: 5px;
+  margin: 10px;
+  border-radius: 5px;
+  display: flex;
+`;
+
+const BotaoAdd = styled.button`
+  margin-bottom: 40px;
+  padding: 4px;
+  border-radius: 5px;
+  display: flex;
+`;
+
+const LogoTopo = styled.img`
+  margin-top: 10px;
+  margin-bottom: 20px;
+`;
+
+const DivPost = styled.div``;
 
 class App extends React.Component {
   state = {
-    arrayPosts: [ 
+    arrayPosts: [
       {
-        nomeUsuario: 'paulinha',
-        fotoUsuario:'https://picsum.photos/50/50/?a=1',
-        fotoPost:'https://picsum.photos/200/150/?a=1'
+        nomeUsuario: "paulinha",
+        fotoUsuario: "https://picsum.photos/50/50/?a=1",
+        fotoPost: "https://picsum.photos/200/150/?a=1",
       },
       {
-        nomeUsuario: 'bya',
-        fotoUsuario: 'https://picsum.photos/50/50/?a=2',
-        fotoPost: 'https://picsum.photos/200/150/?a=4'
+        nomeUsuario: "bya",
+        fotoUsuario: "https://picsum.photos/50/50/?a=2",
+        fotoPost: "https://picsum.photos/200/150/?a=4",
       },
       {
-        nomeUsuario: 'nana',
-        fotoUsuario: 'https://picsum.photos/50/50/?a=3',
-        fotoPost: 'https://picsum.photos/200/150/?a=5'
-      }
-    ]
-  }
+        nomeUsuario: "nana",
+        fotoUsuario: "https://picsum.photos/50/50/?a=3",
+        fotoPost: "https://picsum.photos/200/150/?a=5",
+      },
+    ],
+  };
+
+  adicionaPost = () => {
+    const postNovo = {
+      nomeUsuario: this.state.valorInputNomeUsuario,
+      fotoUsuario: this.state.valorInputFotoUsuario,
+      fotoPost: this.state.valorInputFotoPost,
+    };
+
+    const novosPosts = [postNovo, ...this.state.arrayPosts];
+    this.setState({ arrayDePosts: novosPosts });
+    this.setState({ valorInputNomeUsuario: "" });
+    this.setState({ valorInputFotoUsuario: "" });
+    this.setState({ valorInputFotoPost: "" });
+  };
+
+  onChangeInputNomeUsuario = (event) => {
+    this.setState({ valorInputNomeUsuario: event.target.value });
+  };
+
+  onChangeInputFotoUsuario = (event) => {
+    this.setState({ valorInputFotoUsuario: event.target.value });
+  };
+
+  onChangeInputFotoPost = (event) => {
+    this.setState({ valorInputFotoPost: event.target.value });
+  };
 
   render() {
     const listaDePosts = this.state.arrayPosts.map((post, index, posts) => {
@@ -33,12 +89,31 @@ class App extends React.Component {
         />
       );
     });
-      
-      return (
-        <div className={'app-container'}>
-          {listaDePosts}
+
+    return (
+      <DivAppContainer>
+        <LogoTopo src="https://fontmeme.com/permalink/201017/08e5965c69408e8a7950d5d2ffcb6f9e.png" />
+        <div>
+          <Input
+            value={this.state.valorInputNomeUsuario}
+            onChange={this.onChangeInputNomeUsuario}
+            placeholder={"Usuário"}
+          />
+          <Input
+            value={this.state.valorInputFotoUsuario}
+            onChange={this.onChangeInputFotoUsuario}
+            placeholder={"Foto pessoal"}
+          />
+          <Input
+            value={this.state.valorInputFotoPost}
+            onChange={this.onChangeInputFotoPost}
+            placeholder={"Faça seu post"}
+          />
+          <BotaoAdd onClick={this.adicionaPost}>Adicionar</BotaoAdd>
         </div>
-      );
+        <DivPost>{listaDePosts}</DivPost>
+      </DivAppContainer>
+    );
   }
 }
 
