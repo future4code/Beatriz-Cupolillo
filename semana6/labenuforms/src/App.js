8c8307a7b5cdf1c665b910ea07172428459437bd
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-10-19 14:49:12 -0300

    iniciando React para o LabenuForms

---
+++ semana6/labenuforms/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Etapa1 from "./components/Etapa1";
+import Etapa2 from "./components/Etapa2";
+import Etapa3 from "./components/Etapa3";
+import Etapa4 from "./components/Etapa4";
+
+function App() {
+  <div>
+
+  </div>
+}
+
+export default App;
