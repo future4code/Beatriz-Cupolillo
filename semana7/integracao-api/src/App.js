c9ade2f29d841762de114e2e8cbe36996b656750
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-10-27 15:58:02 -0300

    getUsers e rascunho de layout

--- semana7/integracao-api/src/App.js
+++ semana7/integracao-api/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+  state = {
+    users: [],
+    inputName: "",
+    inputEmail: ""
+  }
+
+  getUser = () => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+        {
+          headers: {
+            Authorization: "beatriz-cupolillo-dumont"
+          }
+        }
+      )
+      .then((response) => {
+        this.setState({users: response.data });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <label>Nome:</label>
+          <input type="text"/>
+          <label>E-mail:</label>
+          <input type="email"/>
+          <button>Salvar</button>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
