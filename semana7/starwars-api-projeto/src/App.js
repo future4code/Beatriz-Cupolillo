ebc7b7ec6c92813f206944933ddd91c0d96156bf
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-10-29 15:56:24 -0300

    inicializando projeto com axios e styled components

---
+++ semana7/starwars-api-projeto/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
