018c49b2eadb0bb27285711efc78eac88c5e69d1
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-11-12 23:48:54 -0300

    versão 1.0 finalizada

--- semana9/projeto-astromatch/src/Components/MatchList/Styled.js
+++ semana9/projeto-astromatch/src/Components/MatchList/Styled.js
@@ -15,9 +15,13 @@ export const ListContainer = styled.div`
 
 export const ClearButton = styled.button`
     font-size: 1em;
-    margin: 1em;
+    margin: 28px 0px 10px 25px;
     padding: 0.25em 1em;
     border-radius: 3px;
     color: palevioletred;
     border: 2px solid palevioletred;
+    background-color: white;
+    &:hover{
+        background-color: #fcf7f7;
+    }
 `
