9677ca5921fefa3a4c102b9252ab5688a6f0006f
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-12-03 20:59:46 -0300

    criando postcard e feed

--- semana12/labeddit/src/Screens/Feed/index.js
+++ semana12/labeddit/src/Screens/Feed/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { useProtectPage } from '../../Hooks/useProtectPage'
+import { useRequestData } from '../../Hooks/useRequestData'
+import { useHistory } from 'react-router-dom';
+import { BASE_URL } from '../../Constants/apiContant'
+import { FeedPageContainer } from './styled'
+import { PostCard } from '../../Constants/PostCard'
+
+
 
 const FeedPage = () => {
     useProtectPage()
+    const history = useHistory()
+    const posts = useRequestData(`${BASE_URL}/posts`, [])
 
-    return <div>FeedPage - As noticias aparecerao aqui</div>
+
+    return (
+        <FeedPageContainer>
+            {posts.map(post => {
+                return <PostCard 
+                key={post.id}
+                title={post.title}
+                username={post.username}
+                text={post.text}
+                commentsCount={post.commentsCount}
+                votesCount={post.votesCount}
+                direction={post.userVoteDirection}
+                />
+            })}
+            {/* <FabStyled color="primary" onClick={() => goToAddPost(history)}>
+                +
+            </FabStyled> */}
+        </FeedPageContainer>
+    )
 }
 
 export default FeedPage;
