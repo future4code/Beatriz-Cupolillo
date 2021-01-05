c85daea54c7341cced828b12d8acaa2f62233829
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2021-01-05 20:35:44 -0300

    resolvido ex 1

--- semana15/exercicios-typescript/src/exercicio1.ts
+++ semana15/exercicios-typescript/src/exercicio1.ts
@@ -2,6 +2,54 @@
 
 const minhaString: string = 5
 
-//ex B - 
+//ex B - para aceitar ambas as opcoes precisa utilizar a | e os dois tipos.
 
 const meuNumero: number | string = "vv"
+
+//ex C - 
+
+const me: {name: string, age: number, favoriteColor: string} = {
+    name: "Beatriz",
+    age: 27,
+    favoriteColor: "blue"
+}
+
+type person = {name: string, age: number, favoriteColor: string}
+
+const Tokio: person = {
+    name: "Tokio",
+    age: 35,
+    favoriteColor: "vermelho"
+}
+
+const Adriana: person = {
+    name: "Adriana",
+    age: 53,
+    favoriteColor: "roxo"
+} 
+
+const Pedro: person = {
+    name: "Pedro",
+    age: 19,
+    favoriteColor: "verde"
+}
+
+//ex e
+
+enum colors {
+    RED = "vermelho",
+    ORANGE = "laranja",
+    YELLOW = "amarelo",
+    GREEN = "verde",
+    BLUE = "azul",
+    INDIGO = "indigo",
+    VIOLET = "violeta"
+}
+
+type newPerson = {name: string, age: number, favoriteColor: colors}
+
+const Beatriz: newPerson = {
+    name: "Beatriz",
+    age: 27,
+    favoriteColor: colors.BLUE
+}
