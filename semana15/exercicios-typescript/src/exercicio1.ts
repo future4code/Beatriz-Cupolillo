326ea7ffda13823561f538b4cf2f5b451a689ddc
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2021-01-05 17:10:21 -0300

    exercicios typescript

---
+++ semana15/exercicios-typescript/src/exercicio1.ts
@@ -0,0 +1,7 @@
+//ex A - O proprio vscode aponta o erro informando que uma variavel string nao aceita numeros como parametro.
+
+const minhaString: string = 5
+
+//ex B - 
+
+const meuNumero: number | string = "vv"
